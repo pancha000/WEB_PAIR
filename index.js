a4c737d690083640b04cde258a3ef72cb5e2d370
pancha000 <panchamd2512@gmail.com>
2025-03-02 11:56:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
-__path = process.cwd();
+const __path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let code = require("./pair");
-
 require("events").EventEmitter.defaultMaxListeners = 500;
-
 app.use("/code", code);
 
 app.use("/", async (req, res, next) => {
@@ -15,7 +13,6 @@ app.use("/", async (req, res, next) => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.listen(PORT, () => {
   console.log(`⏩ Server running on http://localhost:` + PORT);
 });
